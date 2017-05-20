39585a435ede970b02be7bd2c8eeee98a5692b60
Richard Hood <rick@flowmediadesign.com>
2017-05-20 10:41:07 -0400

    config

---
+++ themes/custom/lightup/js/map.js
@@ -0,0 +1,36 @@
+(function ($) {
+    // Add basemap.
+    var baseLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+    });
+
+    // Create map and set center and zoom.
+    var map = L.map('map', {
+        zoomControl: false,
+        scrollWheelZoom: false,
+        center: [42.2570286, -71.8785668],
+        zoom: 8
+    });
+
+    // Add basemap to map.
+    map.addLayer(baseLayer);
+
+    L.Icon.Default.imagePath = '/themes/custom/lightup/images/leaflet/';
+
+    // Add points.
+    function addDataToMap(data, map) {
+        var dataLayer = L.geoJson(data, {
+            onEachFeature: function(feature, layer) {
+                var popupText = feature.properties.name;
+                layer.bindPopup(popupText);
+            }
+        });
+        dataLayer.addTo(map);
+    }
+
+    $.getJSON('/lights', function(data) {
+        addDataToMap(data, map);
+    });
+
+})(jQuery);
+
