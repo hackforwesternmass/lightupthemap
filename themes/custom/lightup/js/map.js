445f4935085248557f2b161698622fa26397ce24
Richard Hood <rick@flowmediadesign.com>
2017-05-28 10:16:19 -0400

    added devel, search kint various map stuff

--- themes/custom/lightup/js/map.js
+++ themes/custom/lightup/js/map.js
@@ -1,9 +1,10 @@
 (function ($) {
+
+    // initial stuff: create map and add base layer
     // Add basemap.
     var baseLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
     });
-
     // Create map and set center and zoom.
     var map = L.map('map', {
         zoomControl: true,
@@ -11,15 +12,54 @@
         center: [42.26, -71.70],
         zoom: 9
     });
-
     // Add basemap to map.
     map.addLayer(baseLayer);
 
+    // examples from here http://leafletjs.com/examples/quick-start/
+    // add a marker
+    var greenIcon = L.icon({
+        iconUrl: '/themes/custom/lightup/images/leaflet/leaf-green.png',
+        //shadowUrl: 'leaf-shadow.png',
+
+        iconSize:     [38, 95], // size of the icon
+        shadowSize:   [50, 64], // size of the shadow
+        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
+
+    var marker = L.marker([42.00, -72.50],{icon:greenIcon });
+    map.addLayer(marker);
+
+    // example of adding a circle
+    var circle = L.circle([41.896539, -71.094021], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 2000
+    });
+    map.addLayer(circle);
+
+    // example of adding a polygon
+    var polygon = L.polygon([
+        [42.10, -72.30],
+        [42.10, -72.25],
+        [42.15, -72.25],
+        [42.15, -72.30]
+    ]);
+    map.addLayer(polygon);
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
+    circle.bindPopup("I am a circle.");
+    polygon.bindPopup("I am a polygon.");
+
+    // images for pins
     L.Icon.Default.imagePath = '/themes/custom/lightup/images/leaflet/';
 
+    // need this for clustering
     var markerClusters = L.markerClusterGroup();
 
-    // Add points.
+    // Add points
     function addDataToMap(data, map) {
         var dataLayer = L.geoJson(data, {
             onEachFeature: function(feature, layer) {
