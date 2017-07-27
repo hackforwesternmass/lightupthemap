7694e2d9794b345c8bc246bbd57c30d55cef5039
Richard Hood <rick@flowmediadesign.com>
2017-07-27 07:56:27 -0400

    added forced https to settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -25,3 +25,25 @@ if (file_exists($local_settings)) {
 }
 
 $settings['hash_salt'] = 'c1Luva8cclI8ixxKSHToOmoRGmEbUwpsnoFF26YjkM8JrBJJvYRA6XzrgPh3f4XyJOFlfRAJ0Q';
+
+if (isset($_SERVER['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli') {
+  // Redirect to https://$primary_domain/ in the Live environment
+  if ($_ENV['PANTHEON_ENVIRONMENT'] === 'live'):
+    /** Replace www.example.com with your registered domain name */
+    $primary_domain = 'www.lightupthemap.com';
+  else:
+    // Redirect to HTTPS on every Pantheon environment.
+    $primary_domain = $_SERVER['HTTP_HOST'];
+  endif;
+  if ($_SERVER['HTTP_HOST'] != $primary_domain
+    || !isset($_SERVER['HTTP_X_SSL'])
+    || $_SERVER['HTTP_X_SSL'] != 'ON' ) {
+    header('HTTP/1.0 301 Moved Permanently');
+    header('Location: '. 'https://'. $primary_domain . $_SERVER['REQUEST_URI']);
+    exit();
+  }
+  // Drupal 8 Trusted Host Settings
+  if (is_array($settings)) {
+    $settings['trusted_host_patterns'] = array('^'. preg_quote($primary_domain) .'$');
+  }
+}
