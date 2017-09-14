575e5ec11aceae4d401642b16952c9186a12baf8
Andrew Pasquale <andrew@elytra.net>
2017-09-14 10:18:31 -0400

    Initial implementation of creating lights through REST.

--- themes/custom/lightup/js/lightup.js
+++ themes/custom/lightup/js/lightup.js
@@ -1,7 +1,6 @@
 // custom js
 
 (function ($) {
-
     $('.action-info').hide();
     $( '.action-title' ).click(function() {
         $(this).next('.action-info').toggle();
@@ -18,6 +17,276 @@
     //         $('#branding').css({'position': 'static', 'top': '0px'});
     //     }
     // });
+
+    var loc; // [lat, lng] of user
+    var city_loc // [lat, lng] of user's city
+
+    $("button.take-action").on("click", function() {
+      if (!logged_in) {
+        $("#takeActionModal").modal();
+      } else {
+        $("#makeACallModal").modal();
+      }
+    });
+
+    $("form[name=address]").on( "submit", function( event ) {
+      event.preventDefault();
+      $('#takeActionModal').modal('hide');
+      $('#makeACallModal').modal('show');
+      var MAPS_API_KEY = 'AIzaSyBuSeH7ueeVuaJs03xIwjtd7hbmShWZ_ew';
+      var OS_API_KEY = '2adfa609-63df-4a8d-bd7c-a243ec2b873f';
+      var address = $("#street").val() + " " + $("#city").val() + " " + $("#state").val();
+      var city = $("#city").val() + " " + $("#state").val();
+      $.ajax({
+        // Geocode the user's address
+        url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + MAPS_API_KEY,
+        type: "GET",
+        dataType: "json",
+        success: function (result) {
+          loc = result.results[0].geometry.location;
+          console.log(loc);
+          // Get the location of the user's city
+          // We'll use this for posting light markers on the map
+          $.ajax({
+            url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + city + '&key=' + MAPS_API_KEY,
+            type: "GET",
+            dataType: "json",
+            success: function (result) {
+              city_loc = result.results[0].geometry.location;
+            }
+          });
+          // Initialize call database
+          //var database = firebase.database();
+          $.ajax({
+            // Lookup user's representatives
+            url: 'https://openstates.org/api/v1/legislators/geo/?lat=' + loc['lat'] + '&long=' + loc['lng'] + '&apikey=' + OS_API_KEY,
+            type: "GET",
+            dataType: "jsonp",
+            success: function (reps) {
+              console.log(reps);
+
+              for (rep of reps) {
+                var div = document.createElement('div')
+                div.className = 'rep';
+                var photo = document.createElement('div');
+                console.log(rep.photo_url);
+                photo.style.backgroundImage = 'url(' + rep.photo_url +')';
+                photo.style.backgroundRepeat = 'no-repeat';
+                photo.className = 'photo';
+                div.appendChild(photo);
+                var innerDiv = document.createElement('div');
+                div.appendChild(innerDiv);
+                var name = document.createElement('h3');
+                name.innerHTML = rep.full_name;
+                name.className = 'name';
+                innerDiv.appendChild(name);
+                var role = document.createElement('div');
+                role.innerHTML = rep.party + (rep.chamber == 'lower' ? ' Representative ' : " Senator ") + 'for the ' + rep.district + ' District';
+                role.className = 'role';
+                innerDiv.appendChild(role);
+                for (office of rep.offices) {
+                  var phone = document.createElement('div');
+                  phone.innerHTML = '<i class="fa fa-lg fa-fw ' + (office.name == 'Capitol office'? ' fa-university' : ' fa-map-marker') + '"></i><span class="office-name">' + office.name +':</span> <button type="button" class="btn btn-primary" data-phone="' + office.phone + '">Call now!</button><span class="phone-number"><a href="tel:' + office.phone + '">' + office.phone + '</a></span>';
+                  phone.className = 'phone';
+                  innerDiv.appendChild(phone);
+                }
+                var website = document.createElement('div');
+                website.innerHTML = '<i class="fa fa-user-circle-o fa-lg fa-fw"></i><a href="' + rep.url + '">Website</a>';
+                website.className = "website";
+                innerDiv.appendChild(website);
+
+                $('#reps').append(div);
+              };
+              var viewmap = document.createElement('div');
+              viewmap.className = 'viewmap';
+              viewmap.innerHTML = 'Done calling? See your light on the map!<br/><button type="button" class="btn btn-primary btn-lg viewmap">View Map</button>';
+              $('#reps').append(viewmap);
+            },
+            error: function () {
+                console.log('There was a problem with the request to geocode the address.');
+            }
+          });
+        },
+        error: function () {
+            console.log('There was a problem with the request to geocode the address.');
+        }
+      });
+    });
+
+    // Click on the call button creates a new light
+    $(document.body).on('click', '.phone .btn', function(){
+      // Hide this button and show the reps phone number
+      $(this).hide();
+      $(this).siblings('.phone-number').show();
+
+      // Create light node
+      $.get('/rest/session/token', function(token) {
+        $.ajax({
+          method: "POST",
+          url: "/entity/node?_format=json",
+          contentType: "application/json",
+          dataType: "json",
+          headers: {
+            "X-CSRF-Token": token
+          },
+          data: JSON.stringify({
+            "langcode": [
+              {
+                "value": "en"
+              }
+            ],
+            "type": [
+              {
+                "target_id": "light",
+                "target_type": "node_type"
+              }
+            ],
+            "title": [
+              {
+                "value": action_title
+              }
+            ],
+            "default_langcode": [
+              {
+                "value": true
+              }
+            ],
+            "body": [],
+            "field_action": [
+              {
+                "target_id": action_nid,
+                "target_type": "node"
+              }
+            ],
+            "field_light_representative": [
+              {
+                "target_id": 113,
+                "target_type": "node"
+              }
+            ],
+            "field_light_senator": [
+            ],
+            "field_map": [
+                {
+                  "lat": city_loc['lat'],
+                  "lng": city_loc['lng']
+                }
+            ]
+          })
+        }).done( function(data) {
+          console.log(data);
+          console.log(data.nid[0].value);
+          anonLights = JSON.parse(localStorage.getItem('anonymousLights'));
+          if (!anonLights) { anonLights = [] };
+          anonLights.push(data.nid[0].value);
+          localStorage.setItem('anonymousLights', JSON.stringify(anonLights));
+          var marker = new L.marker(loc, { riseOnHover: true }).addTo(map);
+        });
+      });
+
+      // Display call counter
+      /*
+      var phone = $(this).data('phone');
+      var callListRef = firebase.database().ref('calls');
+      callListRef.push({'rep': phone });
+
+      var calls = callListRef.equalTo(phone);
+      starCountRef.on('value', function(snapshot) {
+        updateStarCount(postElement, snapshot.val());
+      });
+      */
+    });
+
+    $(document.body).on('click', 'button.viewmap', function(){
+      $('#makeACallModal').modal('hide');
+      var zoom = 10;
+      map.setView(loc, zoom, {pan: {animate: true}});
+      $('#take-action').hide();
+      $('#join-and-share').show();
+    });
+    // Register a new account.
+    // Then login to that account and patch any anonymous lights
+    // that were created before the account was registered.
+    $( "form[name=register]" ).on( "submit", function( event ) {
+      event.preventDefault();
+      $.get('/rest/session/token', function(token) {
+        var name = $('form[name="register"] input[name="name"]').val();
+        var email = $('form[name="register"] input[name="email"]').val();
+        var password = $('form[name="register"] input[name="password"]').val();
+        // Register the new account
+        $.ajax({
+          method: "POST",
+          url: "/user/register?_format=json",
+          contentType: "application/json",
+          headers: {
+            "X-CSRF-Token": token
+          },
+          data: JSON.stringify({
+            "name": { "value": name },
+            "mail": { "value": email },
+            "pass": { "value": password },
+            "field_light_location": { "value": JSON.stringify(city_loc) }
+          })
+        }).success( function(data) {
+          $.get('/rest/session/token', function(token) {
+            // Login to the newly registered account
+            $.ajax({
+              url : "/user/login?_format=json",
+              type : 'POST',
+              contentType: "application/json",
+              headers: {
+                "X-CSRF-Token": token
+              },
+              data: JSON.stringify({
+                'name' : name,
+                'pass' : password,
+              }),
+              dataType : 'json',
+              error : function(data) {
+                console.log('Error ', data.responseText);
+              },
+              success : function(data) {
+                console.log(data);
+                console.log(data.current_user["uid"]);
+                console.log(data.csrf_token);
+                // Update any lights created before the account was registered
+                for (node of JSON.parse(localStorage.getItem('anonymousLights'))) {
+                  jQuery.ajax({
+                    url: '/node/' + node + '?_format=json',
+                    method: 'PATCH',
+                    headers: {
+                      'Content-Type': 'application/json',
+                      'X-CSRF-Token': data.csrf_token
+                    },
+                    data: JSON.stringify({
+                      "uid": [{
+                        "target_id": data.current_user["uid"],
+                        "target_type": "user"
+                      }],
+                      "type": [{
+                        "target_id": "light"
+                      }]
+                    }),
+                    success: function (node) {
+                      console.log(node);
+                      // Successful update
+                      localStorage.setItem('anonymousLights', '');
+                    }
+                  });
+                }
+              }
+            });
+          });
+        });
+      });
+    });
+    // Prevent background scrolling when the modal is open.
+    $('.modal').on('shown.bs.modal', function (e) {
+      $('body').css('overflow','hidden');
+    })
+    $('.modal').on('hidden.bs.modal', function (e) {
+      $('body').css('overflow','auto');
+    })
 
 })(jQuery);
 
@@ -39,7 +308,3 @@ var action = getUrlParameter('action');
 jQuery(document).ready(function($) {
     $("input[value=" + action + "]").prop('checked', true);
 });
-
-
-
-
---
+++ themes/custom/lightup/js/map_actions.js
@@ -0,0 +1,138 @@
+var map; // Global map object
+(function ($) {
+
+    // initial stuff: create map and add base layer
+    // Add base map
+    var baseLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+    });
+    // Create map and set center and zoom.
+        map = L.map('map', {
+        zoomControl: true,
+        scrollWheelZoom: false,
+        center: [42.06, -71.715],
+        zoom: 9
+    });
+    // Add basemap to map.
+    map.addLayer(baseLayer);
+
+    // images for pins
+    L.Icon.Default.imagePath = '/themes/custom/lightup/images/leaflet/';
+
+    // need this for clustering
+    var markerClusters = L.markerClusterGroup();
+    // Create SubGroups.
+    // var lightsMarkerSub = L.featureGroup.subGroup(markerClusters).addTo(map);
+    // var userMarkerSub = L.featureGroup.subGroup(markerClusters).addTo(map);
+    // // For Layers Control.
+    // var overlayMaps = {
+    //     "Lights": lightsMarkerSub,
+    //     "User": userMarkerSub
+    // };
+
+    // Add points
+    function addDataToMap(data, map) {
+        var dataLayer = L.geoJson(data, {
+            onEachFeature: function(feature, layer) {
+                var popupText = feature.properties.name;
+                layer.bindPopup(popupText);
+            }
+        });
+        // this is for no clustering
+        // dataLayer.addTo(map);
+        // this is for clustering
+        markerClusters.addLayer( dataLayer );
+    }
+
+    // add districts
+    function addDistrictsToMap(data, map) {
+        var dataLayer = L.geoJson(data, {
+            onEachFeature: function(feature, layer) {
+                var popupText = feature.properties.REP_DIST;
+                layer.bindPopup(popupText);
+                // set style
+                layer.setStyle({fillColor :'#faca06', fillOpacity: 0.1, color : '#faca06', weight : .2});
+                // mouse over districts
+                layer.on('mouseover', function() {
+                    this.setStyle({fillColor :'#faca06', fillOpacity: 0.2, color : '#faca06', weight : .2 });
+                });
+                layer.on('mouseout', function() {
+                    this.setStyle({fillColor :'#faca06', fillOpacity: 0.1, color : '#faca06', weight : .2});
+                });
+                // this is just a test of if
+                // if(feature.properties.REP_DIST == '1st Barnstable') {
+                //    layer.setStyle({fillColor :'red', fillOpacity: 1})
+                // }
+            }
+        });
+        dataLayer.addTo(map);
+    }
+
+    //var userMarker = L.geoJson(null, userOptions); // DO NOT add to map.
+    //var lightsMarker = L.geoJson(null, lightsOptions); // Same story.
+
+    // get the lights data for logged-in user (not used)
+    // $.getJSON('/json-lights-user', function(data) {
+    //     addDataToMapUser(data, map);
+    // });
+    // get the lights data
+    // this comes from a Drupal view admin/structure/views/view/map_lights_json
+    $.getJSON('/json-lights', function(data) {
+        addDataToMap(data, map);
+    });
+    // get the districts data
+    $.getJSON("/themes/custom/lightup/js/from_pat.geojson",function(data){
+        //L.geoJson( data ).addTo(map);
+        addDistrictsToMap(data, map);
+    });
+
+    map.addLayer( markerClusters );
+    //map.addLayer( markerClustersUser );
+
+
+    // see https://stackoverflow.com/questions/35949424/leaflet-clustering-with-multiple-layers-use-markercluster-layersupport
+
+    // Create a normal Marker Cluster Group.
+    //var mcg = L.markerClusterGroup().addTo(map);
+
+    //var mcg = L.markerClusterGroup().addTo(map);
+
+    // Create SubGroups.
+    //var beerMarkerSub = L.featureGroup.subGroup(mcg).addTo(map);
+    //var wineMarkerSub = L.featureGroup.subGroup(mcg).addTo(map);
+
+    //var beerMarkerSub = L.featureGroup.subGroup(mcg).addTo(map);
+
+    // For Layers Control.
+    // var overlayMaps = {
+    //     "Breweries": beerMarkerSub,
+    //     "Wineries": wineMarkerSub
+    // };
+
+    // That is it! No need to check-in.
+
+    // var beerMarker = L.geoJson(null, beerOptions); // DO NOT add to map.
+    // var wineMarker = L.geoJson(null, wineOptions); // Same story.
+    //
+
+    // var beerMarker = L.geoJson(null, beerOptions); // DO NOT add to map.
+    // $.getJSON('/json-lights-user', function(data) {
+    //     beerMarker.addData(data); // GeoJSON conversion.
+    //     // Then transfer all features into the corresponding sub-group.
+    //     beerMarker.eachLayer(function (layer) {
+    //         layer.addTo(beerMarkerSub);
+    //     });
+    // });
+
+    //
+    // $.getJSON('/json-lights', function(data) {
+    //     wineMarker.addData(data); // GeoJSON conversion.
+    //     // Then transfer all features into the corresponding sub-group.
+    //     wineMarker.eachLayer(function (layer) {
+    //         layer.addTo(wineMarkerSub);
+    //     });
+    // });
+
+    //map.addLayer( mcg );
+
+})(jQuery);
