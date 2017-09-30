cde32f374f9ac5f029fbf5c53b5c2f21ab723b1d
Andrew Pasquale <andrew@elytra.net>
2017-09-29 21:06:43 -0400

    Some styling updates and create new nodes from lights posted while anonymous.

--- themes/custom/lightup/js/lightup.js
+++ themes/custom/lightup/js/lightup.js
@@ -3,6 +3,7 @@ var MAPS_API_KEY = 'AIzaSyBuSeH7ueeVuaJs03xIwjtd7hbmShWZ_ew';
 var OS_API_KEY = '2adfa609-63df-4a8d-bd7c-a243ec2b873f';
 
 (function ($) {
+  console.log(loc);
     $('.action-info').hide();
     $( '.action-title' ).click(function() {
         $(this).next('.action-info').toggle();
@@ -21,7 +22,6 @@ var OS_API_KEY = '2adfa609-63df-4a8d-bd7c-a243ec2b873f';
     // });
 
     $("button.take-action").on("click", function() {
-      $('#take-action').hide();
       if (!logged_in) {
         $("#findYourRepModal").modal();
       } else {
@@ -77,7 +77,144 @@ var OS_API_KEY = '2adfa609-63df-4a8d-bd7c-a243ec2b873f';
       var rep_name = $(this).parent().siblings('.name').html();
 
       // Create light node
+      $.when( postLight(action_title, action_nid, rep_name, city_loc) )
+      .then( function(data) {
+        console.log(data);
+        if (!localStorage.getItem('Drupal.visitor.mail')) {
+          console.log('No mail in storage');
+          var anonLights = [];
+          if (localStorage.getItem('anonymousLights')) {
+            anonLights = JSON.parse(localStorage.getItem('anonymousLights'));
+          }
+          anonLights.push(data.nid[0].value);
+          localStorage.setItem('anonymousLights', JSON.stringify(anonLights));
+        }
+        var marker = new L.marker(loc, { riseOnHover: true }).addTo(map);
+      });
+
+      // Display call counter
+      /*
+      var phone = $(this).data('phone');
+      var callListRef = firebase.database().ref('calls');
+      callListRef.push({'rep': phone });
+
+      var calls = callListRef.equalTo(phone);
+      starCountRef.on('value', function(snapshot) {
+        updateStarCount(postElement, snapshot.val());
+      });
+      */
+    });
+
+    $(document.body).on('click', 'button.viewmap', function(){
+      $('#makeACallModal').modal('hide');
+      var zoom = 10;
+      map.setView(loc, zoom, {pan: {animate: true}});
+      $('#find-your-rep').hide();
+      $('#take-action').hide();
+      $('#join-and-share').show();
+    });
+    // Register a new account.
+    // Then login to that account and patch any anonymous lights
+    // that were created before the account was registered.
+    $( "form[name=register]" ).on( "submit", function( event ) {
+      event.preventDefault();
       $.get('/rest/session/token', function(token) {
+        var name = $('form[name="register"] input[name="name"]').val();
+        var email = $('form[name="register"] input[name="email"]').val();
+        var password = $('form[name="register"] input[name="password"]').val();
+        // Register the new account
+        $.ajax({
+          method: "POST",
+          url: "/user/register?_format=json",
+          contentType: "application/json",
+          headers: {
+            "X-CSRF-Token": token
+          },
+          data: JSON.stringify({
+            "name": { "value": name },
+            "mail": { "value": email },
+            "pass": { "value": password },
+            "field_location": [{
+              "lat": JSON.stringify(loc["lat"]),
+              "lng": JSON.stringify(loc["lng"])
+            }],
+            "field_city": [{
+              "lat": JSON.stringify(city_loc["lat"]),
+              "lng": JSON.stringify(city_loc["lng"])
+            }]
+          })
+        }).done( function(data) {
+          $.get('/rest/session/token', function(token) {
+            // Login to the newly registered account
+            $.ajax({
+              url : "/user/login?_format=json",
+              type : 'POST',
+              contentType: "application/json",
+              headers: {
+                "X-CSRF-Token": token
+              },
+              data: JSON.stringify({
+                'name' : name,
+                'pass' : password,
+              }),
+              dataType : 'json',
+            }).done( function(data) {
+              // Account successfully created
+              $('#registerModal').modal('hide');
+              $('#join').hide();
+              $('#accountCreatedModal').modal();
+              // Update any lights created before the account was registered
+              if (localStorage.getItem('anonymousLights')) {
+                var anonLights = JSON.parse(localStorage.getItem('anonymousLights'));
+                for (node of anonLights) {
+                  jQuery.ajax({
+                    url: '/node/' + node + '?_format=json',
+                    method: 'GET',
+                    headers: {
+                      'Content-Type': 'application/json',
+                      'X-CSRF-Token': data.csrf_token
+                    },
+                  }).done(function (node) {
+                      postLight(node.title[0].value, node.field_action[0].target_id, node.field_representative_text[0].value, {"lat": node.field_map[0].lat, "lng": node.field_map[0].lng})
+                      .done( function(data) {
+                        console.log(data);
+                      });
+                      console.log(node);
+                      // Successful update
+                      localStorage.setItem('anonymousLights', '');
+                  })
+                  .fail( function() {
+
+                  })
+                }
+              }
+            }).fail( function(data) {
+              console.log(data);
+            });
+          }).fail( function(data) {
+            console.log(data);
+          });
+        }).fail( function(data) {
+          console.log(data);
+        });
+      });
+    });
+    // Prevent background scrolling when the modal is open.
+    $('.modal').on('shown.bs.modal', function (e) {
+      $('body').css('overflow','hidden');
+    })
+    $('.modal').on('hidden.bs.modal', function (e) {
+      $('body').css('overflow','auto');
+    })
+    $('.map-overlay .close').on('click', function(e) {
+      $('.map-overlay').hide();
+    })
+
+    function postLight(action_title, action_nid, rep_name, city_loc) {
+      var def = $.Deferred();
+      console.log("1");
+      $.get('/rest/session/token', function(token) {
+        console.log("2");
         $.ajax({
           method: "POST",
           url: "/entity/node?_format=json",
@@ -127,130 +264,12 @@ var OS_API_KEY = '2adfa609-63df-4a8d-bd7c-a243ec2b873f';
                 }
             ]
           })
-        }).done( function(data) {
-          console.log(data);
-          console.log(data.nid[0].value);
-          anonLights = JSON.parse(localStorage.getItem('anonymousLights'));
-          if (!anonLights) { anonLights = [] };
-          anonLights.push(data.nid[0].value);
-          localStorage.setItem('anonymousLights', JSON.stringify(anonLights));
-          var marker = new L.marker(loc, { riseOnHover: true }).addTo(map);
-        });
+        }).done( function(response){
+          def.resolve(response);
+        })
       });
-
-      // Display call counter
-      /*
-      var phone = $(this).data('phone');
-      var callListRef = firebase.database().ref('calls');
-      callListRef.push({'rep': phone });
-
-      var calls = callListRef.equalTo(phone);
-      starCountRef.on('value', function(snapshot) {
-        updateStarCount(postElement, snapshot.val());
-      });
-      */
-    });
-
-    $(document.body).on('click', 'button.viewmap', function(){
-      $('#makeACallModal').modal('hide');
-      var zoom = 10;
-      map.setView(loc, zoom, {pan: {animate: true}});
-      $('#find-your-rep').hide();
-      $('#join-and-share').show();
-    });
-    // Register a new account.
-    // Then login to that account and patch any anonymous lights
-    // that were created before the account was registered.
-    $( "form[name=register]" ).on( "submit", function( event ) {
-      event.preventDefault();
-      $.get('/rest/session/token', function(token) {
-        var name = $('form[name="register"] input[name="name"]').val();
-        var email = $('form[name="register"] input[name="email"]').val();
-        var password = $('form[name="register"] input[name="password"]').val();
-        // Register the new account
-        $.ajax({
-          method: "POST",
-          url: "/user/register?_format=json",
-          contentType: "application/json",
-          headers: {
-            "X-CSRF-Token": token
-          },
-          data: JSON.stringify({
-            "name": { "value": name },
-            "mail": { "value": email },
-            "pass": { "value": password },
-            "field_location": [{
-              "lat": JSON.stringify(loc["lat"]),
-              "lng": JSON.stringify(loc["lng"])
-            }],
-            "field_city": [{
-              "lat": JSON.stringify(city_loc["lat"]),
-              "lng": JSON.stringify(city_loc["lng"])
-            }]
-          })
-        }).success( function(data) {
-          $.get('/rest/session/token', function(token) {
-            // Login to the newly registered account
-            $.ajax({
-              url : "/user/login?_format=json",
-              type : 'POST',
-              contentType: "application/json",
-              headers: {
-                "X-CSRF-Token": token
-              },
-              data: JSON.stringify({
-                'name' : name,
-                'pass' : password,
-              }),
-              dataType : 'json',
-              error : function(data) {
-                console.log('Error ', data.responseText);
-              },
-              success : function(data) {
-                console.log(data);
-                console.log(data.current_user["uid"]);
-                console.log(data.csrf_token);
-                // Update any lights created before the account was registered
-                for (node of JSON.parse(localStorage.getItem('anonymousLights'))) {
-                  jQuery.ajax({
-                    url: '/node/' + node + '?_format=json',
-                    method: 'PATCH',
-                    headers: {
-                      'Content-Type': 'application/json',
-                      'X-CSRF-Token': data.csrf_token
-                    },
-                    data: JSON.stringify({
-                      "uid": [{
-                        "target_id": data.current_user["uid"],
-                        "target_type": "user"
-                      }],
-                      "type": [{
-                        "target_id": "light"
-                      }]
-                    }),
-                    success: function (node) {
-                      console.log(node);
-                      // Successful update
-                      localStorage.setItem('anonymousLights', '');
-                    }
-                  });
-                }
-              }
-            });
-          });
-        });
-      });
-    });
-    // Prevent background scrolling when the modal is open.
-    $('.modal').on('shown.bs.modal', function (e) {
-      $('body').css('overflow','hidden');
-    })
-    $('.modal').on('hidden.bs.modal', function (e) {
-      $('body').css('overflow','auto');
-    })
-    $('.map-overlay .close').on('click', function(e) {
-      $('.map-overlay').hide();
-    })
+      return def.promise();
+    }
 
     function lookupLegislators(lat, lng) {
       $.ajax({
